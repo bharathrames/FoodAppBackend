95cf0a67496b12182bb145a2edbefe313a33e87a
bharathrames <bharath91505@gmail.com>
2023-08-22 08:47:23 +0530

    index

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ global.foodData = require('./db')(function call(err, data, CatData) {
   const app = express()
   const port = 5000
   app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Origin", "https://recepieappes.netlify.app");
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
